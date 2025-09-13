e1c8b6a7da4e7c046183fc0076a7dd94bfbbeef1
andreweggleston <egglestonandrew927@gmail.com>
2025-09-12 21:40:47 -0400

    move to 25.05 and fix numpy issues

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "go-stop dev environment";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
@@ -60,6 +60,9 @@
             pkgs.nodejs_20
             pythonEnv
             frontend
+            pkgs.gcc-unwrapped
+            pkgs.libz
+            pkgs.stdenv.cc.cc.lib
           ];
         };
       }
